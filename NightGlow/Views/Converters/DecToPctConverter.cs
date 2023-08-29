49e6e0e40ac1c1b372efbab1de17b6b2350ac503
Tom Bayley <thomas.bayley1@gmail.com>
2023-08-29 01:08:36 +0100

    show brightness as 0-100 in sliders instead of 0-1

---
+++ NightGlow/Views/Converters/DecToPctConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System;
+using System.Windows.Data;
+using System.Diagnostics;
+
+namespace NightGlow.Views.Converters;
+
+public class DecToPctConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is double doubleValue)
+            return (int)Math.Round(doubleValue * 100);
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (double)value / 100;
+    }
+}
